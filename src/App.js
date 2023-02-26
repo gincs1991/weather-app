80e216a87a5c9561b112d83363aa687ed1356b98
Anca Ioana Aparaschivei <aancaioana@gmail.com>
2023-02-26 17:19:36 +0200

    working app

--- src/App.js
+++ src/App.js
@@ -4,21 +4,21 @@ import Select from 'react-select';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons'
 
-// const options = [
-//   { value: 'Bucharest', label: 'Bucharest' },
-//   { value: 'Bacau', label: 'Bacau' },
-//   { value: 'Sighisoara', label: 'Sighisoara' },
-//   { value: 'Sydney', label: 'Sydney' }
-// ];
+const options = [
+  { value: 'Bucharest', label: 'Bucharest' },
+  { value: 'Bacau', label: 'Bacau' },
+  { value: 'Sighisoara', label: 'Sighisoara' },
+  { value: 'Sydney', label: 'Sydney' }
+];
 
-const currentURL = "https://api.weatherapi.com/v1/current.json";
-const searchURL = "http://api.weatherapi.com/v1/search.json";
+const URL = "https://api.weatherapi.com/v1/current.json";
+// const searchURL = "http://api.weatherapi.com/v1/search.json";
 const API_KEY = "34ddc8f39463427ba9d165646232502";
 
 function App() {
   const [city, setCity] = useState(options[0]);
   const [cityData, setCityData] = useState(null);
-  const [search, setSearch] = useState();
+  // const [search, setSearch] = useState();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -48,7 +48,7 @@ function App() {
         wind_dir, 
         pressure_mb, 
         humidity, 
-        condition: { icon }
+        condition: { icon, text }
       } 
     } = cityData;
 
@@ -57,7 +57,7 @@ function App() {
         <h1>{`${name}, ${country}`}</h1>
         <p>Temperature, Celsius: {temp_c}</p>
         {is_day ? <FontAwesomeIcon icon={faSun} /> : <FontAwesomeIcon icon={faMoon} />}
-        <img src={icon} />
+        <img alt={text} src={icon} />
         <p>Wind: {wind_kph}</p>
         <p>Wind direction: {wind_dir}</p>
         <p>Pressure: {pressure_mb}</p>
